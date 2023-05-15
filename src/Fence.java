1ab65d85529c9c30d8b776fac828184cfc800ad1
RobinMeneust <robin.meneust@hotmail.fr>
2023-05-15 18:14:17 +0200

    Added Fence Class

--- src/Fence.java
+++ src/Fence.java
@@ -1,3 +1,30 @@
 public class Fence {
-	// TO-DO
+	private int length;
+	private Orientation orientation;
+	private Point start;
+	private Point end;
+
+	public Fence(int length, Orientation orientation, Point start, Point end) {
+		this.length = length;
+		this.orientation = orientation;
+		this.start = start;
+		this.end = end;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public Orientation getOrientation() {
+		return orientation;
+	}
+
+	public Point getStart() {
+		return start;
+	}
+
+	public Point getEnd() {
+		return end;
+	}
+	
 }
