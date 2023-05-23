8298193dedcaeac9116d4c8277ca0e2773f65d3a
RobinMeneust <robin.meneust@hotmail.fr>
2023-05-23 14:19:14 +0200

    Split launch function in console mode to make it more readable

--- src/main/java/abstraction/GameConsole.java
+++ src/main/java/abstraction/GameConsole.java
@@ -46,10 +46,127 @@ public class GameConsole extends GameAbstract {
     public GameConsole(Player[] players, int nbMaxTotalFences, int nbRows, int nbCols, HashMap<Integer,Player> playersPawnIndex, Pawn[] pawns, int currentPlayerIndex) throws InvalidNumberOfPlayersException {
         super(players, nbMaxTotalFences, nbRows, nbCols, playersPawnIndex, pawns, currentPlayerIndex);
     }
+
+    /**
+     * Ask the user if he wants to move, place a fence or save a game
+     * 
+     * @param canPlaceFence Indicates if the user is allowed to place a fence. If he can it's equals to true and if he can't it's false
+     * @return User's choice
+     */
+
+    private String getUserActionChoice(boolean canPlaceFence) {
+        String response = "";
+
+        do {
+            if(canPlaceFence) {
+                System.out.println("What is your next action ? ('m' (move) or 'f' (fence) or 's' (save))");
+            } else {
+                System.out.println("What is your next action ? ('m' (move) or 's' (save))");
+            }
+            response = CYPath.scanner.nextLine();
+            response = response.toUpperCase();
+            
+            if(response.matches("S(AVE)?")) {
+                boolean isValidSave = false;
+                do {
+                    try {
+                        System.out.println("What is the name of your save file ? (without extension) :");
+                        String fileName = CYPath.scanner.nextLine();
+
+                        SaveDataInJSONFile saveDataObject = new SaveDataInJSONFile(this.getBoard().getNbRows(), this.getBoard().getNbCols(), this.getBoard().getFencesArray(), this.getNbMaxTotalFences(), this.getBoard().getPawnsArray(), this.getCurrentPawnIndex());
+                        saveDataObject.save(fileName);
+                        isValidSave = true;
+                        System.out.println("Game successfully saved");
+                    } catch (FileNameIsDuplicateException e) {
+                        System.err.println("Error: the name of the file is already used, write a new name save file :");
+                    } catch (IOException e) {
+                        System.err.println("Error: there is an error during saving game, write a new name save file :");
+                    }
+                } while (!isValidSave);
+            }
+        }while(!response.matches("M(OVE)?") && !(response.matches("F(ENCE)?") || !canPlaceFence));
+
+        return response;
+    }
+
+    /**
+     * Ask where the user wants to move its pawn and move it if's a valid position
+     */
+
+    private void playerMovePawn() {
+        Point point = null;
+        boolean isPawnPosValid = false;
+        System.out.println("Where do you want to go ?");
+
+        do {
+            try {
+                point = Point.choosePoint();
+                isPawnPosValid = this.getBoard().movePawn(this.getCurrentPawn().getId(), point);
+                if(!isPawnPosValid) {
+                    System.out.println("The pawn can't move here\nTry again.");
+                }
+            } catch (NumberFormatException ignored) {
+
+            } catch (IncorrectPawnIndexException e) {
+                System.err.println("ERROR: Pawn index is incorrect. Check the number of players and the number of pawns and see if they are equals");
+                System.exit(-1);
+            }
+        } while(!isPawnPosValid);
+    }
+
+    /**
+     * Ask where the user what should be the orientation of the given fence and change it to this new value
+     * 
+     * @param fence Fence whose orientation is changed
+     */
+
+    private void playerChangeFenceOrientation(Fence fence) {
+        String orientation = null;
+        boolean isValidOrientation = false;
+
+        do {
+            System.out.println("What is the orientation of your fence ? (H(ORIZONTAL) or V(ERTICAL))");
+            orientation = this.getBoard().chooseOrientation();
+            isValidOrientation = this.getBoard().isValidOrientation(orientation);
+
+            if(!isValidOrientation) {
+                System.out.println(orientation+" is not a valid orientation\nTry again.");
+            }
+        }while(!isValidOrientation);
+        fence.setOrientation(orientation);
+    }
+
+    /**
+     * Ask where the user wants to place the given fence and place it if's a valid fence
+     * 
+     * @param fence Fence placed
+     */
+
+    private void playerPlaceFence(Fence fence) {
+        Point point = null;
+        boolean isFenceValid = false;
+
+        try {
+            do {
+                System.out.println("Where do you want to put your fence ? (X,Y)");
+                point = Point.choosePoint();
+                fence.setStart(point);
+                isFenceValid = this.getBoard().placeFence(this.getCurrentPawn().getId(), fence);
+                if(!isFenceValid) {
+                    System.out.println("The fence can't be placed here (Starting point:" + fence.getStart() + ").\nTry again.");
+                }
+            } while(!isFenceValid);
+        } catch (IncorrectPawnIndexException e) {
+            // If this exception is thrown the pawn ids are incorrect
+            System.err.println(e);
+            System.exit(-1);
+        }
+    }
     
     /**
      * Launch the current game in console mode
      */
+
 
     public void launch() {
 		// The game is now in progress
@@ -58,91 +175,35 @@ public class GameConsole extends GameAbstract {
         String response = "";
         Pawn currentPawn = null;
 
-        try {
+        try{
             // Play until there is a winner
             while(this.getBoard().getWinner() == -1){
                 this.getBoard().displayBoard(DisplayType.NO_COORD);
 
                 System.out.println("Turn of player: " +  this.getCurrentPlayer());
                 currentPawn = this.getCurrentPawn();
-                System.out.println("You have "+currentPawn.getAvailableFences()+ " fences remaining.\n");
-                if(currentPawn.getAvailableFences() == 0){
-                    response = "move";
-                    System.out.println("You don't have any fence remaining. You can only move.");
-                } else{
-                    do {
-                        System.out.println("What is your next action ? ('m' (move) or 'f' (fence) or 's' (save))");
-                        response = CYPath.scanner.nextLine();
-                        response = response.toUpperCase();
-                        
-                        if(response.matches("S(AVE)?")) {
-                            boolean isValidSave = false;
-                            do {
-                                try {
-                                    System.out.println("What is the name of your save file ? (without extension) :");
-                                    String fileName = CYPath.scanner.nextLine();
 
-                                    SaveDataInJSONFile saveDataObject = new SaveDataInJSONFile(this.getBoard().getNbRows(), this.getBoard().getNbCols(), this.getBoard().getFencesArray(), this.getNbMaxTotalFences(), this.getBoard().getPawnsArray(), this.getCurrentPawnIndex());
-                                    saveDataObject.save(fileName);
-                                    isValidSave = true;
-                                } catch (FileNameIsDuplicateException e) {
-                                    System.err.println("Error: the name of the file is already used, write a new name save file :");
-                                } catch (IOException e) {
-                                    System.err.println("Error: there is an error during saving game, write a new name save file :");
-                                }
-                            } while (!isValidSave);
-                        }
-                    }while(!response.matches("M(OVE)?") && !response.matches("F(ENCE)?"));
+                if(currentPawn.getAvailableFences() == 0){
+                    System.out.println("You don't have any fence remaining. You can only move.");
+                    response = getUserActionChoice(false);
+                } else{
+                    System.out.println("You have "+currentPawn.getAvailableFences()+ " fences remaining.\n");
+                    response = getUserActionChoice(true);
                 }
         
                 if(response.matches("M(OVE)?")){
                     this.getBoard().displayBoard(DisplayType.COORD_CELL);
-
                     List<Point> listPossibleMoves = this.getBoard().getCurrentPossibleMoves();
-
                     System.out.println("Those are the cells where your pawn can move to:");
                     System.out.println(listPossibleMoves);
                     
-                    Point point = null;
-                    boolean isPawnPosValid = false;
-                    System.out.println("Where do you want to go ?");
-
-                    do {
-                        try {
-                            point = Point.choosePoint();
-                            isPawnPosValid = this.getBoard().movePawn(this.getCurrentPawn().getId(), point);
-                            if(!isPawnPosValid) {
-                                System.out.println("The pawn can't move here\nTry again.");
-                            }
-                        } catch (NumberFormatException ignored) {}
-                    } while(!isPawnPosValid);
+                    playerMovePawn();
                 } else if (response.matches("F(ENCE)?")) {
                     this.getBoard().displayBoard(DisplayType.COORD_LINE);
-                    boolean isFenceValid = false;
-
-                    String orientation = "";
                     Fence fence = new Fence(this.getBoard().getFenceLength());
-                    Point point = null;
-                    do {
-                        System.out.println("What is the orientation of your fence ? (H(ORIZONTAL) or V(ERTICAL))");
-                        orientation = this.getBoard().chooseOrientation();
-                        isFenceValid = this.getBoard().isValidOrientation(orientation);
 
-                        if(!isFenceValid) {
-                            System.out.println(orientation+" is not a valid orientation\nTry again.");
-                        }
-                    }while(!isFenceValid);
-                    fence.setOrientation(orientation);
-
-                    do {
-                        System.out.println("Where do you want to put your fence ? (X,Y)");
-                        point = Point.choosePoint();
-                        fence.setStart(point);
-                        isFenceValid = this.getBoard().placeFence(this.getCurrentPawn().getId(), fence);
-                        if(!isFenceValid) {
-                            System.out.println("The fence can't be placed here (Starting point:" + fence.getStart() + ").\nTry again.");
-                        }
-                    } while(!isFenceValid);
+                    playerChangeFenceOrientation(fence);
+                    playerPlaceFence(fence);
                 }
                 this.endPlayerTurn();
             }
@@ -150,8 +211,10 @@ public class GameConsole extends GameAbstract {
             Player playerWinner = this.getBoard().getPawn(this.getBoard().getWinner()).getPlayer();
             System.out.println("The winner is "+playerWinner);
             this.setState(GameState.FINISHED);
+            
         } catch (IncorrectPawnIndexException e) {
-            System.err.println("ERROR: Pawn index is incorrect. Check the number of players and the number of pawns and see if they are equals");
+            // If this exception is thrown the pawn ids are incorrect
+            System.err.println(e);
             System.exit(-1);
         }
 	}
